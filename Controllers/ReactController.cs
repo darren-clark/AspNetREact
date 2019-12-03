ffacee76c1545d617f620b5587077ee0a372fc9a
Darren Clark <darren.clark@avianis.com>
2019-12-03 13:59:41 -0800

    wip

---
+++ Controllers/ReactController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetReact.Controllers
+{
+    public class ReactController : Controller
+    {
+        // GET: React
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult App()
+        {
+            return View();
+        }
+    }
+}
---
+++ Scripts/es6/webpack.config.js
@@ -0,0 +1,26 @@
+﻿"use strict";
+
+var path = require("path");
+var WebpackNotifierPlugin = require("webpack-notifier");
+var BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "../es6-dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    devtool: "inline-source-map",
+    plugins: [new WebpackNotifierPlugin(), new BrowserSyncPlugin()]
+};
